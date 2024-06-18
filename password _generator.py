e21229594c684df253a5878eb53af719de0a98d9
Racem Dammak <142255050+racemdammak@users.noreply.github.com>
2024-06-18 10:43:18 +0100

    code enhanced

--- password _generator.py
+++ password _generator.py
@@ -1,34 +1,24 @@
+import string
 import random
+
 pwd = False
 while pwd == False:
     pwd_len = int(input("the length of the password (must be betwwen 8 and 16): "))
-    if 8<=pwd_len<=16 :
+    if 8 <= pwd_len <= 16 :
         pwd = True
 numbers = input("do you want numbers in your password? (Y/N): ")
 uppercases = input("do you want uppercase letters in your password? (Y/N): ")
 symboles = input("do you want symboles in your password? (Y/N): ")
-password = ""  
+password = "" 
 
-sym_list = ["&","@","-","_","#","{","}","(",")","[","]"]
-pwd_list = list()
-
-for i in range(97,123):
-        pwd_list.append(str(chr(i)))
+char_set = string.ascii_lowercase
 
 if numbers.upper() == 'Y':
-    num = str(random.randint(0,9))
-    pwd_list.append(num)
+    char_set += string.digits
 if uppercases.upper() == 'Y':
-    for i in range (65,91):
-        pwd_list.append(str(chr(i)))
+    char_set += string.ascii_uppercase
 if symboles.upper() == 'Y':
-    pwd_list.extend(sym_list)
+    char_set += string.punctuation
 
-i = 1
-while i <= pwd_len :
-    indice = random.randint(0,len(pwd_list))
-    password += pwd_list[indice]
-    i += 1
-
+password = password.join(random.choices(char_set, k=pwd_len))
 print(password)
-
